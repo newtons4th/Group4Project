1a62867094193327a34587b9a39ddb042756258f
Kevin <kkwak@unomaha.edu>
2019-10-29 00:04:07 -0500

    added homepage

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
+def home(request):
+    return render(request, 'shop/product/home.html',
+        {'shop': home})
 
 def product_list(request, category_slug=None):
     category = None
